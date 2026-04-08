49598492e2cbe3f3ea71bcdfcb44667ac8ab3834
Gabriel Henrique De Melo Costa <130712878+binelzim@users.noreply.github.com>
2026-04-08 23:28:57 +0000

    Aula Firebase

--- AULA-FIREBASE/ex1Firebase/App.js
+++ AULA-FIREBASE/ex1Firebase/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import LoginScreen from './src/screens/LoginScreen';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
+import UserListScreen from './src/screens/UserListScreen';
+import EditUserScreen from './src/screens/EditUserScreen';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Bem-vindo" component={WelcomeScreen} />
+      <Drawer.Screen name="Lista de Usuários" component={UserListScreen} />
+    </Drawer.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" component={MyDrawer} options={{ headerShown: false }} />
+        <Stack.Screen name="EditUser" component={EditUserScreen} options={{ title: 'Editar Usuário' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
--- AULA-FIREBASE/ex1Firebase/src/config/firebaseConfig.js
+++ AULA-FIREBASE/ex1Firebase/src/config/firebaseConfig.js
@@ -1,2 +1,19 @@
-import { initializeApp } from 'firebase';
-import { getAuth } from 'firebase'
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBVFwyaJjS3BrRpu2LA8Orev3roDFf5TKM",
+  authDomain: "unipam-aula-rafael.firebaseapp.com",
+  projectId: "unipam-aula-rafael",
+  storageBucket: "unipam-aula-rafael.firebasestorage.app",
+  messagingSenderId: "659378409124",
+  appId: "1:659378409124:web:aa2e1e1e71091e2b8de031",
+  measurementId: "G-EQJQ5MFDTC"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getFirestore(app);
